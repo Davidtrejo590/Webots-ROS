4e731773d05f7039d75ab07f0b98da25943aac84
davidtrejo <luisdavidtorrestrejo@gmail.com>
2022-01-05 13:17:49 -0600

    Fill point cloud

--- catkin_ws/src/bring_up_web/src/object_detect.cpp
+++ catkin_ws/src/bring_up_web/src/object_detect.cpp
@@ -4,6 +4,7 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/ml/kmeans.h>
 
 
 /**
@@ -18,8 +19,6 @@ void objectDetectCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
   cloud.height = 1;
   cloud.points.resize(cloud.width * cloud.height);
 
-
-
   int j = 0;
   void* p = (void*)(&msg-> data[0]);                                            // POINTER TO FIRST DATA IN THE POINT CLOUD 2
 
@@ -31,7 +30,7 @@ void objectDetectCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
     float z = *((float*)(p + 8));
     p += msg -> point_step;
 
-    // FILTERING DATA
+    // FILTERING DATA & FILLING NEW POINT CLOUD
     if( (isinf(x) or isinf(y) or isinf(z)) != true){
       if ( (x > 1.0 or x < -1.0) and (y > -1.5) and (z > 2.0 or z < -2.0)){
         cloud.points[j].x = x;
@@ -42,13 +41,25 @@ void objectDetectCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
     } 
   }
 
-  std::cout << "----- START -----" << std::endl;
-  for (size_t i = 0; i < cloud.points.size(); i++)
-  {
-    std::cout << cloud.points[i].x << cloud.points[i].y << cloud.points[i].z << std::endl;
-  }
-  std::cout << "----- END -----" << std::endl;
+  // K MEANS
+  // pcl::Kmeans real(cloud.points.size(), 3);
+  // real.setClusterSize(3);
+  // for (size_t i = 0; i < cloud.points.size(); i++){
+  //   std::vector<float> data(3);
+  //   data[0] = cloud.points[i].x;
+  //   data[1] = cloud.points[i].y;
+  //   data[2] = cloud.points[i].z;
+  //   real.addDataPoint(data);
+  // }
 
+  // real.kMeans();
+  // pcl::Kmeans::Centroids centroids = real.get_centroids();
+  // std::cout << "-----START-----" << std::endl;
+  // for (size_t i = 0; i < centroids.size(); i++){
+  //   // CENTROIDS
+  //   std::cout << "x: " << centroids[i][0] << "  y: " << centroids[i][1] << "  z: " << centroids[i][2] << std::endl;
+  // }
+  // std::cout << "-----END-----" << std::endl;
 
 }
 
@@ -65,3 +76,4 @@ int main(int argc, char **argv)
 
   return 0;
 }
+
