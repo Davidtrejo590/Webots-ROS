da641219434c235fe7f2e84bc203f093aa328fd0
davidtrejo <luisdavidtorrestrejo@gmail.com>
2022-02-04 08:54:24 -0600

    Init EKF

--- catkin_ws/src/bring_up_web/scripts/object_velocity.py
+++ catkin_ws/src/bring_up_web/scripts/object_velocity.py
@@ -4,17 +4,11 @@ import math
 import rospy
 import time
 from geometry_msgs.msg import PoseArray
-from std_msgs.msg import Float32
 
 # GLOBAL VARIABLES
 current_centroids = []
 
-# PUBLISHERS
-pub_vel = rospy.Publisher('/centroid_vel', Float32, queue_size=10)
-
 def callback_object_pose(msg):
-
-    global actual_centroids
 
     last_centroids = current_centroids.copy()                                                           # GET LAST CENTROIDS
     start_time = time.time()                                                                            # START TIME
@@ -26,27 +20,45 @@ def callback_object_pose(msg):
 
     end_time = time.time()                                                                              # END TIME
     final_time = end_time-start_time                                                                    # GET TOTAL TIME BETWEEN LAST-CURRENT
-    # print('ACTUAL', actual_centroids)
+    # print('ACTUAL', current_centroids)
 
     # CALCULATE DISTANCE BETWEEN LAST & CURRENT
     if last_centroids:
         print('----')
         for (lc, cc) in zip(last_centroids, current_centroids):
-            distance = math.sqrt( (lc[0]-cc[0])**2 + (lc[1]-cc[1])**2 + (lc[2]-cc[2])**2 )
-            vel = distance / final_time
-            pub_vel.publish(vel)
-            print('[last, actual, d, v]; ', [lc, cc, distance, vel] )
+            if lc[0] != 0.0:
+                distance = math.sqrt( (lc[0]-cc[0])**2 + (lc[1]-cc[1])**2 + (lc[2]-cc[2])**2 )
+                vel = distance / final_time
+                # print('[last, actual, d, v]; ', [lc, cc, distance, vel] )
+
+
+# KALMAN FILTER
+def predict():
+    print('PREDICT')
+    # g -> g (x_t, u)
+    # G -> j(g)
+    # sigma = G * sigma * Gt + Q
+
+
+def update():
+    print('UPDATE')
+    # z -> MEDICIONES REALES
+    # z_hat = h(x, S)   -> LECTURAS PREVISTAS
+    # H = J(h)
+
+    # k = sigma * Ht * (1/ (H * sigma * Ht + R))            -> KALMAN GAIN
+
+    # ACTUALIZAR VECTOR DE ESTADO Y MATRIZ DE COVARIANZA
+    # x = x + k( z - z_hat)
+    # sigma = (I - (k * H )) * sigma
 
 def main():
-
-    global velocity, speeds
-    print('Object Velocity node ...')
+    print('Kalman Filter Node...')
     rospy.init_node('object_velocity')
     rate = rospy.Rate(10)
 
     # SUBSCRIBERS
-    rospy.Subscriber('/centroid_pose', PoseArray, callback_object_pose)
-    
+    rospy.Subscriber('/object_pose', PoseArray, callback_object_pose)
     rospy.spin()
 
 
