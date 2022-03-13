c13cbeb55498c7900a3187522003aad9d2424f73
davidtrejo <luisdavidtorrestrejo@gmail.com>
2022-03-13 09:51:11 -0600

    Remove unnecessary sensors

--- catkin_ws/src/bring_up_web/controllers/ros_car_controller/ros_car_controller.py
+++ catkin_ws/src/bring_up_web/controllers/ros_car_controller/ros_car_controller.py
@@ -35,12 +35,12 @@ lidar.enable(TIME_STEP)
 lidar.enablePointCloud()
 
 # INIT GPS
-gps = GPS('gps')
-gps.enable(TIME_STEP)
+# gps = GPS('gps')
+# gps.enable(TIME_STEP)
 
 # INIT GYRO
-gyro = Gyro('gyro')
-gyro.enable(TIME_STEP)
+# gyro = Gyro('gyro')
+# gyro.enable(TIME_STEP)
 
 # INIT KEYBOARD
 keyboard = Keyboard()
@@ -116,17 +116,17 @@ def main():
   msg_point_cloud.is_bigendian = False
 
   # GPS MESSAGE
-  msg_gps = NavSatFix()
-  msg_gps.header.stamp = rospy.Time.now()
-  msg_gps.header.frame_id = 'gps_link'
-  # msg_gps.position_covariance = NavSatFix.COVARIANCE_TYPE_KNOWN
-  # msg_gps.status.service = NavSatStatus.SERVICE_GPS
+  # msg_gps = NavSatFix()
+  # msg_gps.header.stamp = rospy.Time.now()
+  # msg_gps.header.frame_id = 'gps_link'
+  # # msg_gps.position_covariance = NavSatFix.COVARIANCE_TYPE_KNOWN
+  # # msg_gps.status.service = NavSatStatus.SERVICE_GPS
   
 
   # GYRO MESSAGE
-  msg_gyro = Imu()
-  msg_gyro.header.stamp = rospy.Time.now()
-  msg_gyro.header.frame_id = 'gyro_link'
+  # msg_gyro = Imu()
+  # msg_gyro.header.stamp = rospy.Time.now()
+  # msg_gyro.header.frame_id = 'gyro_link'
   
 
   # PUBLISHERS
@@ -145,21 +145,21 @@ def main():
 
     msg_image.data = camera.getImage()                                                  # GET IMAGE DATA FROM CAMERA
     msg_point_cloud.data = lidar.getPointCloud(data_type='buffer')                      # GET POINT CLOUD FROM LIDAR
-    msg_gyro.angular_velocity.x = gyro.getValues()[0]                                   # GET X COMPONENT FROM GYRO
-    msg_gyro.angular_velocity.y = gyro.getValues()[1]                                   # GET Y COMPONENT FROM GYRO
-    msg_gyro.angular_velocity.z = gyro.getValues()[2]                                   # GET Z COMPONENT FROM GYRO
-    msg_gps.latitude = gps.getValues()[0]                                               # GET X COMPONENT FROM GPS  
-    msg_gps.longitude = gps.getValues()[1]                                              # GET Y COMPONENT FROM GPS  
-    msg_gps.altitude = gps.getValues()[2]                                               # GET Z COMPONENT FROM GPS 
+    # msg_gyro.angular_velocity.x = gyro.getValues()[0]                                   # GET X COMPONENT FROM GYRO
+    # msg_gyro.angular_velocity.y = gyro.getValues()[1]                                   # GET Y COMPONENT FROM GYRO
+    # msg_gyro.angular_velocity.z = gyro.getValues()[2]                                   # GET Z COMPONENT FROM GYRO
+    # msg_gps.latitude = gps.getValues()[0]                                               # GET X COMPONENT FROM GPS  
+    # msg_gps.longitude = gps.getValues()[1]                                              # GET Y COMPONENT FROM GPS  
+    # msg_gps.altitude = gps.getValues()[2]                                               # GET Z COMPONENT FROM GPS 
     
     
     pub_camera_data.publish(msg_image)                                                  # PUBLISHING IMAGE MESSAGE
     pub_point_cloud.publish(msg_point_cloud)                                            # PUBLISHING POINTCLOUD2 MESSAGE
-    pub_imu_gyro.publish(msg_gyro)                                                      # PUBLISHING IMU MESSAGE
-    pub_nav_gps.publish(msg_gps)                                                        # PUBLISHING NAVSATFIX MESSAGE
+    # pub_imu_gyro.publish(msg_gyro)                                                      # PUBLISHING IMU MESSAGE
+    # pub_nav_gps.publish(msg_gps)                                                        # PUBLISHING NAVSATFIX MESSAGE
     
     rate.sleep()
-    pass
+
  
 
 if __name__ == "__main__":
